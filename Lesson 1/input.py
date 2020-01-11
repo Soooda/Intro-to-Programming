e63d790ce77b7dac8e2bbbe3b7162a7ef046d35c
Soooda <kongliangxm@foxmail.com>
2020-01-11 19:23:15 +0800

    Lesson1 Lab + Resources upload

---
+++ Lesson 1/input.py
@@ -0,0 +1,2 @@
+name = input('你叫啥？')
+print('我叫' + name)
---
+++ Lesson 1/work.py
@@ -0,0 +1,7 @@
+time = input('工作时间：')
+time = int(time)
+efficiency = input('工作效率: ')
+efficiency = int(efficiency)
+work = time * efficiency
+work = str(work)
+print('工作总量 = ' + work) 
