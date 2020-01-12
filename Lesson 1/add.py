ac1f59985d997f5837858fb7858cfd1d7bd5003b
Soooda <kongliangxm@foxmail.com>
2020-01-12 22:22:25 +0800

    Upload Lesson1-lab resources

---
+++ Lesson 1/add.py
@@ -0,0 +1,12 @@
+# Ask the user to input the first number
+n1 = input('Enter the first number: ')
+n1 = int(n1)
+
+# Ask the user to input the second number
+n2 = input('Enter the second number: ')
+n2 = int(n2)
+
+# Calculate
+n3 = n1 + n2
+
+print(n3)
--- Lesson 1/work.py
+++ Lesson 1/work.py
@@ -1,7 +1,10 @@
-time = input('工作时间：')
+time = input('工作时间：') #10
+# 把字符转变成可以计算的数字
 time = int(time)
-efficiency = input('工作效率: ')
+efficiency = input('工作效率: ') #20
+# 把字符转变成可以计算的数字
 efficiency = int(efficiency)
 work = time * efficiency
+# 把可以计算的数字转变成字符
 work = str(work)
-print('工作总量 = ' + work) 
+print('工作总量 = ' + work)
